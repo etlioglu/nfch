255db05dcb2201737763d71932447e8896370973
H. Emre Etlioglu <emre.etlioglu@gmail.com>
2025-03-16 09:12:45 +0100

    differential abundance app added to main.py

--- src/nfch/main.py
+++ src/nfch/main.py
@@ -6,20 +6,22 @@ uv run --active src/nfch/main.py --help
 Resources:
 https://sarahglasmacher.com/how-to-build-python-package-uv/
 https://www.datacamp.com/tutorial/python-uv
+
+
+Add --save_merged_fastq to the rnaseq nf_settings.json
 """
 
 import typer
 
+from nfch.diffabun import app as diffabun_app
 from nfch.project_init import app as project_app
 from nfch.rnaseq import app as rnaseq_app
-
-# from nfch.diffab import app as diffab_app
 
 app: typer.Typer = typer.Typer()
 
 app.add_typer(typer_instance=project_app, name="project")
 app.add_typer(typer_instance=rnaseq_app, name="rnaseq")
-# app.add_typer(diffab, name="differentialabundance")
+app.add_typer(typer_instance=diffabun_app, name="differentialabundance")
 
 
 if __name__ == "__main__":
